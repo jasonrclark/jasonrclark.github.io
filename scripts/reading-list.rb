b7c45207a29aa90757146cf35953924b0c8e7bc8
Jason R. Clark <jason@jasonrclark.net>
2025-05-18 18:51:54 -0700

    Recommend through issues

--- scripts/reading-list.rb
+++ scripts/reading-list.rb
@@ -1,22 +1,44 @@
 #! /usr/bin/env ruby
 
 new_books = ""
+recommendations = []
 
 lines = ARGV[0].lines.reject do |line|
   line.strip.empty?
 end
 
-lines = lines.each_slice(2).each do |(title, author)|
-  title = title.strip
-  author = author.strip.gsub(/^Book by /, "")
-  author = author.strip.gsub(/^Novel by /, "")
-  new_books += "  <li><i>#{title}</i> - #{author}</li>\n"
+lines = lines.each_slice(2).each do |(first_line, second_line)|
+  if first_line.downcase.start_with?("+recommend")
+    recommendations << second_line
+  else
+    title = first_line.strip
+    author = second_line.strip.gsub(/^Book by /, "")
+    author = author.strip.gsub(/^Novel by /, "")
+    new_books += "  <li><i>#{title}</i> - #{author}</li>\n"
+  end
 end
 
 original_content = File.read("./reading.html")
 
 new_content = original_content.sub(/<ol.*>/) do |header|
-  "#{header}\n#{new_books.chomp}"
+  if new_books.empty?
+    header
+  else
+    "#{header}\n#{new_books.chomp}"
+  end
+end
+
+recommended_lines = recommendations.map do |recommend|
+  new_content.match(/^.*#{recommend}.*$/)
+end
+
+recommended_lines.each do |line|
+  original = line.to_s
+
+  replacement = original.sub("<li>", "<li><b>")
+  replacement = replacement.sub("</li>", "</b></li>")
+
+  new_content = new_content.sub(original, replacement)
 end
 
 File.open("./reading.html", "w") do |file|
